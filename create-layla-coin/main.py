d3c30ddcb24031cd160044e7faca80faf5983104
B Haney <43055154+Bhaney44@users.noreply.github.com>
2021-06-21 08:56:16 -0700

    Update main.py

--- create-layla-coin/main.py
+++ create-layla-coin/main.py
@@ -3,14 +3,13 @@ from config import *
 from algosdk.v2client import algod
 from algosdk import account, mnemonic
 from algosdk.future.transaction import write_to_file
-from algosdk.future.transaction import AssetConfigTxn, AssetTransferTxn
-from util import sign_and_send, balance_formatter
+from algosdk.future.transaction import AssetConfigTxn
 from algosdk.future.transaction import AssetTransferTxn
-
+from util import sign_and_send
+from util import balance_formatter
 
 # Client
-# client = algod.AlgodClient(algod_token, algod_address)
-client = algod.AlgodClient("", algod_address, headers={'User-Agent': 'DoYouLoveMe?'})
+client = algod.AlgodClient(algod_token, algod_address)
 
 # Create function.
 # Returns an unsigned txn object and writes the unsigned transaction object to a file for offline signing. 
@@ -36,19 +35,6 @@ def optin(passphrase=None):
 	else:
 		write_to_file([txns], "optin.txn")
 
-def transfer(passphrase=None):
-    amount = 6000
-    params = client.suggested_params()
-    txn = AssetTransferTxn(sender=creator_address, sp=params, receiver=receiver_address, amt=amount, index=asset_id)
-    if passphrase:
-        txinfo = sign_and_send(txn, passphrase, client)
-        formatted_amount = balance_formatter(amount, asset_id, client)
-        print("Transferred {} from {} to {}".format(formatted_amount, 
-            creator_address, receiver_address))
-        print("Transaction ID Confirmation: {}".format(txinfo.get("tx")))
-    else:
-        write_to_file([txns], "transfer.txn")
-
 # Checks the asset balance for the specific address and asset id.
 def check_holdings(asset_id, address):
 	account_info = client.account_info(address)
@@ -71,8 +57,6 @@ def optin(passphrase=None):
         print("Opted in to asset ID: {}".format(asset_id))
     else:
         write_to_file([txns], "optin.txn")
-	
-from util import balance_formatter
 
 # Checks the asset balance for the specific address and asset id.
 def check_holdings(asset_id, address):
@@ -84,7 +68,6 @@ def check_holdings(asset_id, address):
             print("Account {} has {}.".format(address, balance_formatter(amount, asset_id, client)))
             return
     print("Account {} must opt-in to Asset ID {}.".format(address, asset_id))
-
 
 # Creates an unsigned transfer transaction for the specified asset id, to the specified address, for the specified amount.
 def transfer(passphrase=None):
